6b7c509616a2c968bc9c98d30a961add6f1370f3
andiFebianto123 <andifebianto123@gmail.com>
2022-03-08 17:47:34 +0700

    normalize middleware custom route backpack

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -33,10 +33,10 @@ Route::group(
 
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
-    // 'middleware' => array_merge(
-    //     (array) config('backpack.base.web_middleware', 'web'),
-    //     (array) config('backpack.base.middleware_key', 'admin')
-    // ),
+    'middleware' => array_merge(
+        (array) config('backpack.base.web_middleware', 'web'),
+        (array) config('backpack.base.middleware_key', 'admin')
+    ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
 

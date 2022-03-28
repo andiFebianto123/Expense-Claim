fc6907d453c00db6ae6b3e5b238410e4fed3a9d6
Kevin Djajadinata <kevin@rectmedia.com>
2022-03-28 11:37:11 +0700

    set config password when applicaition boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+       
     }
 
     /**
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        config(['mail.mailers.smtp.password' => Config::where('key', 'Email Password')->first()->value ?? null]);
     }
 }

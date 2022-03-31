c16cc3ddadcafcb55ee5c8cbb1381efcba6e3468
Kevin Djajadinata <kevin@rectmedia.com>
2022-03-31 14:14:43 +0700

    fix limit startDate in expense claim request

--- app/Http/Controllers/Admin/ExpenseApproverGoaDetailCrudController.php
+++ app/Http/Controllers/Admin/ExpenseApproverGoaDetailCrudController.php
@@ -381,7 +381,7 @@ class ExpenseApproverGoaDetailCrudController extends CrudController
             'label' => 'Date',
             'date_picker_options' => [
                 'format' => 'dd M yyyy',
-                'startDate' => Carbon::now()->subMonth()->startOfMonth()->format('d-m-Y'),
+                'startDate' => Carbon::now()->startOfMonth()->subMonth()->format('d-m-Y'),
                 'endDate' => Carbon::now()->format('d-m-Y'),
             ]
         ]);
